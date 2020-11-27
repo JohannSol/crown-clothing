198ad864e0d05f8f644a621b2eee4838f8f8e188
johann43 <emman.soliano@gmail.com>
2020-11-27 11:09:54 +0800

    updating app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './css/App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage =()=> (
-  <div>
-    <h1>This is the HATS page!!</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   // this is where you initialize all the routes that will be referred to by LINK
@@ -14,7 +9,8 @@ function App() {
     <div>
       <Switch> 
         <Route exact path='/' component = { HomePage } /> 
-        <Route exact path='/hats' component = { HatsPage } /> 
+        <Route exact path='/shop' component = { ShopPage } /> 
+
       </Switch>
     </div>
   );
